099bd2e307e74f92ac80543a4d5b96e70f24e8de
Phillip D. Alderman <phillip.alderman@okstate.edu>
2019-09-11 15:18:22 -0500

    Adding function that comblines tiers of the SIMULATION CONTROLS section of a file X. Used in read_filex()

---
+++ R/combine_simulation_controls.R
@@ -0,0 +1,21 @@
+combine_simulation_controls <- function(sim_ctrl_in){
+
+  begin <- sim_ctrl_in %>%
+    map_lgl(~{all(c('N','GENERAL') %in% colnames(.))}) %>%
+    which()
+
+  end <- begin %>%
+    {tail(.,-1) - 1} %>%
+    {c(.,length(sim_ctrl_in))}
+
+
+  sim_ctrl_out <- 1:length(begin) %>%
+    map(function(i){
+      combined <- sim_ctrl_in[begin[i]:end[i]] %>%
+        reduce(combine_tiers)
+      return(combined)
+    }) %>%
+    reduce(combine_tiers)
+
+  return(sim_ctrl_out)
+}
