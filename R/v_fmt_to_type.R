a7e50bf2fe05bb2f8390bc6813cf9a90ff48978c
Phillip D. Alderman <phillip.alderman@okstate.edu>
2024-02-19 12:00:55 -0600

    Cleaning up code and updating .Rbuildignore for pending 0.0.9 release

--- R/v_fmt_to_type.R
+++ R/v_fmt_to_type.R
@@ -1,7 +1,7 @@
 v_fmt_to_type <- function(v_fmt, vnames = NULL){
   if(is.null(vnames)) vnames <- names(v_fmt)
   v_fmt[vnames] |>
-    DSSAT:::extr_substr("[a-z]") |>
+    extr_substr("[a-z]") |>
     sapply(\(.x) switch(.x,
                         "f" = "numeric",
                         "s" = "character"))
